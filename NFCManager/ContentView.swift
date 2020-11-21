7318eb90af32664b9e775aae358f20fcdfdb7063
Aitor <aitorzubizarreta@yahoo.es>
2020-11-21 15:15:35 +0100

    Initial Commit.

---
+++ NFCManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFCManager
+//
+//  Created by Aitor Zubizarreta on 21/11/2020.
+//  Copyright © 2020 Aitor Zubizarreta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
